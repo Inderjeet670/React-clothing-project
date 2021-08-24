29cecd5b44bf49ea37729976e2dfeeddb6c62af2
Inderjeet <jeetinder670@gmail.com>
2021-08-24 20:55:11 +0530

    set up scss and some structure to the app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
+import HomePage from './homepage.component.jsx'
 function App() {
   return (
     <div>
-      hello world
+      <HomePage/>
     </div>
   );
 }
+
+
 
 export default App;
